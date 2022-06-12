e63a1a066c8ff8790b34af43d964660fff5ccb03
laalex <laalex512@gmail.com>
2022-06-12 21:09:58 +0300

    add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет*/
+
+Console.WriteLine("Insert number of day");
+int num = int.Parse(Console.ReadLine());
+
+if (num<1 || num>7)
+{
+    Console.WriteLine("Error");
+}
+else
+{
+    if (num>5) Console.WriteLine("Yes");
+    else Console.WriteLine("No");
+}
