1cff7389e6089677a068205df2fe82d58f7e99a9
laalex <laalex512@gmail.com>
2022-06-12 20:49:52 +0300

    add task1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.WriteLine("Insert 3-digit number");
+int num = int.Parse(Console.ReadLine());
+
+if (num<100 || num>999)
+{
+    Console.WriteLine("Error. Number isn't 3-digit");
+}
+else
+{
+    int secondDigit = (num/10)%10;
+    Console.WriteLine($"second digit: {secondDigit}");
+}
